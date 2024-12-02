40fa72b854d59b69cc8f664a45c357f987a7a198
wanjunjie <wanjunjie@junrunrenli.com>
2024-12-03 00:38:23 +0800

    学习递归和回溯

---
+++ 26-贪心和分治/src/com/one/divideandconquer/MaxSubArray.java
@@ -0,0 +1,52 @@
+package com.one.divideandconquer;
+
+/**
+ * @author one
+ * @description 最大连续子串
+ * @date 2024-12-2
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        return maxSubArray(nums, 0,  nums.length);
+    }
+
+    public int maxSubArray(int[] nums, int left, int right) {
+        if (right - left < 2) {
+            return nums[left];
+        }
+        int middle = (left + right) >> 1;
+        int lmax = Integer.MIN_VALUE;
+        int lsum  = 0;
+        for (int i = middle - 1; i >= left; i--) {
+            lsum += nums[i];
+            lmax = Math.max(lmax, lsum);
+        }
+
+        int rmax = Integer.MIN_VALUE;
+        int rsum = 0;
+        for (int i = middle; i < right; i++) {
+            rsum += nums[i];
+            rmax = Math.max(rmax, rsum);
+        }
+
+        int leftMax = maxSubArray(nums, left, middle);
+        int rightMax = maxSubArray(nums, middle, right);
+        return Math.max(lmax + rmax, Math.max(leftMax, rightMax));
+    }
+
+    private static int mthod1(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end < nums.length; end++) {
+                sum += nums[end];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+}
