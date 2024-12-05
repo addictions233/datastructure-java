6af482dd9771498ce691f199948a9f3f55ca00db
wanjunjie <wanjunjie@junrunrenli.com>
2024-12-05 08:34:06 +0800

    学习动态规划

---
+++ 27-动态规划/src/com/one/dynamicprogramming/coins/CoinChange.java
@@ -0,0 +1,21 @@
+package com.one.dynamicprogramming.coins;
+
+/**
+ * @author one
+ * @description Leetcode 322 零钱兑换
+ * @date 2024-12-5
+ */
+public class CoinChange {
+
+    public int coins(int n) {
+        if (n < 1) {
+            return Integer.MAX_VALUE;
+        }
+        if (n == 25 || n == 20 || n == 5 || n == 1) {
+            return 1;
+        }
+        int min1 = Math.min(coins(n - 25),  coins(n - 20));
+        int min2 = Math.min(coins(n - 5),  coins(n - 1));
+        return Math.min(min1, min2) + 1;
+    }
+}
