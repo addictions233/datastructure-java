5cdae1d4435b1c0d556e22eeec18566fb5293e55
wanjunjie <wanjunjie@junrunrenli.com>
2025-01-20 16:47:00 +0800

    学习动态规划

---
+++ 27-动态规划/src/com/one/leetcode/FindNumberOfLIS.java
@@ -0,0 +1,15 @@
+package com.one.leetcode;
+
+/**
+ * @description: 673. 最长递增子序列的个数
+ * 给定一个未排序的整数数组 nums ， 返回最长递增子序列的个数 。
+ * 注意 这个数列必须是 严格 递增的。
+ * @author: wanjunjie
+ * @date: 2025/01/20
+ */
+public class FindNumberOfLIS {
+
+    public int findNumberOfLIS(int[] nums) {
+        return 0;
+    }
+}
